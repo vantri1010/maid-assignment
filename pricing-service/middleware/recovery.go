9e1e96b21e8d4a2a134303412896c67bd84f12bb
Nguyen Van Tri <vantri1010@gmail.com>
2024-08-09 02:03:20 +0700

    pricing-service

---
+++ pricing-service/middleware/recovery.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"booking-service/model"
+	"github.com/gin-gonic/gin"
+)
+
+func Recover() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				c.Header("Content-Type", "application/json")
+
+				if appErr, ok := err.(*model.AppError); ok {
+					c.AbortWithStatusJSON(appErr.StatusCode, appErr)
+					panic(err)
+					return
+				}
+
+				appErr := model.ErrInternal(err.(error))
+				c.AbortWithStatusJSON(appErr.StatusCode, appErr)
+				panic(err)
+				return
+			}
+		}()
+
+		c.Next()
+	}
+}
